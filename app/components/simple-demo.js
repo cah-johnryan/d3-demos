e81b99e3c5b58319b31a73dee898c18703a88f5c
John Ryan <john.ryan@cardinalhealth.com>
2020-06-10 16:44:58 -0400

    Adding axis lines

--- app/components/simple-demo.js
+++ app/components/simple-demo.js
@@ -3,18 +3,36 @@ import { action } from '@ember/object';
 import * as d3 from "d3";
 
 export default class SimpleDemoComponent extends Component {
-  chartHeight = 200;
-  chartWidth = 450;
+  svgHeight = 400;
+  svgWidth = 450;
+
+  margin = { top: 10, right: 40, bottom: 30, left: 30 };
+  chartHeight = this.svgHeight - this.margin.top - this.margin.bottom;
+  chartWidth = this.svgWidth - this.margin.left - this.margin.right;
 
   @action
   async getDataAndLoadChart() {
-    let svg = d3.select("#simple-demo-container");
+    let svg = d3.select("#simple-demo-container")
+      .append('svg')
+      .attr('width', this.svgWidth)
+      .attr('height', this.svgHeight)
+      .append('g')
+      .attr('transform', `translate(${this.margin.left},${this.margin.top})`);
 
     let x = d3.scaleLinear()
       .domain([0, 100])
-      .range([0, 450]);
+      .range([0, this.chartWidth]);
 
-    svg.call(d3.axisBottom(x));
+    svg.append('g')
+      .attr('transform', `translate(0,${this.chartHeight})`)
+      .call(d3.axisBottom(x));
+
+    let y = d3.scaleLinear()
+      .domain([0, 100])
+      .range([this.chartHeight, 0]);
+
+    svg.append('g')
+      .call(d3.axisLeft(y));
 
     svg.append("circle")
       .attr("cx", 100).attr("cy", 100).attr("r", 40).style("fill", "blue");
