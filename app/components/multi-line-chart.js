d7597389e597de5df4460f86ba778b2846293ae5
John Ryan <john.ryan@cardinalhealth.com>
2020-06-10 11:42:12 -0400

    Retrieving data sucessfully

--- app/components/multi-line-chart.js
+++ app/components/multi-line-chart.js
@@ -1,4 +1,14 @@
 import Component from '@glimmer/component';
+import {action} from '@ember/object';
 
 export default class MultiLineChartComponent extends Component {
+  unemploymentData;
+
+  @action
+  async createChart() {
+    let response = await fetch('http://localhost:4200/multi-line-chart/unemployment.tsv');
+    this.unemploymentData = await response.text();
+
+  }
+
 }
