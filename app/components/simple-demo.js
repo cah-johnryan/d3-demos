1a2c10dba1f3ae9047ce94725d2658b48d2c85ae
John Ryan <john.ryan@cardinalhealth.com>
2020-06-10 16:04:43 -0400

    stepping forward

--- app/components/simple-demo.js
+++ app/components/simple-demo.js
@@ -3,14 +3,24 @@ import { action } from '@ember/object';
 import * as d3 from "d3";
 
 export default class SimpleDemoComponent extends Component {
+  chartHeight = 200;
+  chartWidth = 450;
+
   @action
   async getDataAndLoadChart() {
-    let svg = d3.select("#simple-demo-container")
+    let svg = d3.select("#simple-demo-container");
+
+    let x = d3.scaleLinear()
+      .domain([0, 100])
+      .range([0, 450]);
+
+    svg.call(d3.axisBottom(x));
+
     svg.append("circle")
-      .attr("cx", 2).attr("cy", 2).attr("r", 40).style("fill", "blue");
+      .attr("cx", 100).attr("cy", 100).attr("r", 40).style("fill", "blue");
     svg.append("circle")
-      .attr("cx", 140).attr("cy", 70).attr("r", 40).style("fill", "red");
+      .attr("cx", 225).attr("cy", 100).attr("r", 40).style("fill", "red");
     svg.append("circle")
-      .attr("cx", 300).attr("cy", 100).attr("r", 40).style("fill", "green");
+      .attr("cx", 350).attr("cy", 100).attr("r", 40).style("fill", "green");
   }
 }
