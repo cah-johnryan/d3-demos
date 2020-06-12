30b20f5ab5132c20f444f120f7ecc3fd53d1f241
John Ryan <john.ryan@cardinalhealth.com>
2020-06-12 11:29:30 -0400

    wip

--- app/components/simple-time-series.js
+++ app/components/simple-time-series.js
@@ -6,60 +6,80 @@ import * as moment from "moment";
 // https://observablehq.com/@sdaas/d3-timeseries
 // https://observablehq.com/@mbostock/global-temperature-trends
 export default class SimpleTimeSeriesComponent extends Component {
-  viewportHeight = 400;
-  viewportWidth = 450;
+  d3Config = {
+    viewportHeight: 400,
+    viewportWidth: 900,
+    margin: { top: 30, right: 30, bottom: 30, left: 30 }
+  };
 
-  margin = { top: 10, right: 40, bottom: 30, left: 30 };
-  height = this.viewportHeight - this.margin.top - this.margin.bottom;
-  width = this.viewportWidth - this.margin.left - this.margin.right;
+  constructor() {
+    super(...arguments);
+    this.d3Config.height = this.d3Config.viewportHeight - this.d3Config.margin.top - this.d3Config.margin.bottom
+    this.d3Config.width = this.d3Config.viewportWidth - this.d3Config.margin.left - this.d3Config.margin.right
+  }
+
+  xMapperGenerator(d3, d3Config, dataSeries) {
+    return d3.scaleTime()
+      .domain(d3.extent(dataSeries, d => d.date))
+      .range([d3Config.margin.left, d3Config.viewportWidth - d3Config.margin.right]);
+  }
+
+  xAxis(d3, d3Config, g, xMapper) {
+    return g.attr("transform", `translate(0,${d3Config.viewportHeight - d3Config.margin.bottom})`).call(
+      d3
+        .axisBottom(xMapper)
+        .ticks(10)
+        .tickSizeOuter(0)
+    );
+  }
+
+  yMapperGenerator(d3, d3Config, dataSeries) {
+    return d3.scaleLinear()
+      .domain(d3.extent(dataSeries, d => d.value))
+      .range([d3Config.viewportHeight - d3Config.margin.bottom, d3Config.margin.top]);
+  }
+
+  yAxis(d3, d3Config, g, yMapper) {
+    return g.attr("transform", `translate(${d3Config.margin.left},0)`).call(
+      d3
+        .axisLeft(yMapper)
+        .ticks(5)
+        .tickSizeOuter(0)
+    );
+  }
 
   @action
   async getDataAndLoadChart() {
 
     // Create fake data
     // let data = [{ x: 10, y: 20 }, { x: 40, y: 90 }, { x: 80, y: 50 }]
-    let data = this.generateFakeTimeSeries();
+    const seedTime = moment();
+    const dataSeries1 = this.generateFakeTimeSeries(seedTime);
 
-    let svg = d3.select("#simple-time-series-container")
+    const svg = d3.select("#simple-time-series-container")
       .append('svg')
-      .attr('width', this.viewportWidth)
-      .attr('height', this.viewportHeight);
+      .attr('width', this.d3Config.viewportWidth)
+      .attr('height', this.d3Config.viewportHeight);
 
-    // set a margin around the chart for the axis rulers to sit
-    svg
-      .append('g')
-      .attr('transform', `translate(${this.margin.left},${this.margin.top})`);
-
-    // set up the x axis
-    let xMapper = d3.scaleUtc()
-      .domain(d3.extent(data, d => d.date))
-      .range([0, this.width]);
-
-    svg.append('g')
-      .attr('transform', `translate(0,${this.height})`)
-      .call(d3.axisBottom(xMapper).ticks(10).tickSizeOuter(0));
-
-    // set up the y axis
-    let yMapper = d3.scaleLinear()
-      .domain(d3.extent(data, d => d.value))
-      .range([this.height, 0]);
-
-    svg.append('g')
-      .call(d3.axisLeft(yMapper).ticks(5).tickSizeOuter(0));
+    const xMapper = this.xMapperGenerator(d3, this.d3Config, dataSeries1);
+    const yMapper = this.yMapperGenerator(d3, this.d3Config, dataSeries1);
 
     svg.selectAll('whatever')
-      .data(data)
+      .data(dataSeries1)
       .enter()
       .append('circle')
       .attr('cx', (d => xMapper(d.date)))
       .attr('cy', (d => yMapper(d.value)))
       .attr('r', 3);
+
+    svg.append('g').call((g) => this.xAxis(d3, this.d3Config, g, xMapper));
+    svg.append('g').call((g) => this.yAxis(d3, this.d3Config, g, yMapper));
   }
 
-  generateFakeTimeSeries() {
+  generateFakeTimeSeries(seedTime) {
     const data = [];
-    let d = moment();
-    for (let i = 0, v = 2; i < 24; ++i) {
+    let d = seedTime;
+    for (let i = 0, v = 2; i < 48; ++i) {
       v += Math.random() - 0.5;
       v = Math.max(Math.min(v, 4), 0);
       data.push({
@@ -67,9 +87,8 @@ export default class SimpleTimeSeriesComponent extends Component {
         value: v
       });
 
-      d = d.add(1, 'hour');
+      d = d.add(-1, 'hour');
     }
-    console.log(data);
     return data;
   }
 }
